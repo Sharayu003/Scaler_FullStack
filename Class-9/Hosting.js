308385d9332b7e2c02964118ff53db4b4e92a887
Sharayu Yeole <sharayuyeole38@gmail.com>
2025-09-28 14:50:52 +0530

    Push

---
+++ Class-9/Hosting.js
@@ -0,0 +1,10 @@
+//console.log(a)
+printName()
+// let a = 4 -> you cannot access a before initialization
+// var a = 4 -> output -> undefine 
+
+// const printName = function printName(){
+//     console.log('my name is Sharayu')
+// } -> output -> you cannot access printName before initialization 
+
+// temporal Dead zone -> user try to access value of variable before its initialization that value is keep inside Temporal Dead Zone
---
+++ Class-9/function.js
@@ -0,0 +1,11 @@
+function serveBeverage(drink, quantity){
+    console.log('I want ' + quantity + ' '+ drink)
+}
+
+serveBeverage('coffe', 3)
+
+let sayhi = function(){
+    console.log('hi')
+}
+
+sayhi()
