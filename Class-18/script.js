dc2332a0817dce1fe874f435afb7831770a29292
Sharayu Yeole <sharayuyeole38@gmail.com>
2025-11-12 20:20:54 +0530

    commit

--- Class-18/script.js
+++ Class-18/script.js
@@ -1,6 +1,6 @@
 const addBtn = document.querySelector('.add-btn')
-
 const modalCont = document.querySelector('.modal-cont')
+const mainCont = document.querySelector('.main-cont')
 modalCont.style.display ='none'
 let addtaskFlag = false
 
@@ -15,3 +15,29 @@ addBtn.addEventListener('click',function(){
     }
 
 })
+
+// Generating Tickets
+
+modalCont.addEventListener('keydown', function(e){
+    let key = e.key
+    console.log(key)
+    if(key ==='Shift'){
+        createTicket()
+    }
+})
+
+function createTicket(){
+    let ticketCont = document.createElement('div')
+    ticketCont.setAttribute('class','ticket-cont')
+    ticketCont.innerHTML = ` <div class="ticket-cont">
+            <div class="ticket-color lightblue"></div>
+            <div class="ticket-id">1234</div>
+            <div class="ticket-task">Random Task</div>
+            <div class="ticket-lock"><i class="fa-solid fa-lock"></i></div>
+         </div>`;
+
+         mainCont.appendChild(ticketCont)
+         modalCont.style.display ='none'
+         
+}
+
